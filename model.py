b045009012a165777abe8c0d588f9e075fc86819
snehavardhan <snehavardhank@gmail.com>
2023-12-22 13:04:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -38,3 +38,33 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x= x + (self.pe[: :x.shape[1], :]).requires_grad_(False)
         return self.dropout(X)
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float= 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameters(torch.ones(features))
+        self.bias = nn.Parameters(torch.zeros(features))
+
+    def forward(self, x):
+        #X: (batch,seq_len, hidden_size)
+        # Keeep the dimensions for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) #(batch, seq_len,1)
+        #keep the dimensions for broadcasting
+        std = x.std(dim = -1, keepdim = True)  # (batch, seq_len, 1)
+        #eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout:float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
